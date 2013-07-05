8bd493a9786e1407a3e3780fe3fe92912b2e9df6
Daniel Diaz <diaz.carrete@facebook.com>
2013-07-05 22:11:15 +0200

    expanded poem

--- snaplets/stochastic/sample_poem.js
+++ snaplets/stochastic/sample_poem.js
@@ -1,2 +1,15 @@
-[ "verse a", "verse b" ]
+["Du temps que la Nature en sa verve puissante",
+ "Concevait chaque jour des enfants monstrueux,",
+ "J'eusse aimé vivre auprès d'une jeune géante,",
+ "Comme aux pieds d'une reine un chat voluptueux.",
+ "J'eusse aimé voir son corps fleurir avec son âme",
+ "Et grandir librement dans ses terribles jeux;",
+ "Deviner si son coeur couve une sombre flamme",
+ "Aux humides brouillards qui nagent dans ses yeux;",
+ "Parcourir à loisir ses magnifiques formes;",
+ "Ramper sur le versant de ses genoux énormes,",
+ "Et parfois en été, quand les soleils malsains,",
+ "Lasse, la font s'étendre à travers la campagne,",
+ "Dormir nonchalamment à l'ombre de ses seins,",
+ "Comme un hameau paisible au pied d'une montagne."]
 
